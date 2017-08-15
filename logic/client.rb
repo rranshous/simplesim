04d61693b6063adaddf0c1d1b4573eeb5b383e57
Robby Ranshous <rranshous@gmail.com>
2017-08-15 15:48:24 -0400

    working on drawing

--- logic/client.rb
+++ logic/client.rb
@@ -1,6 +1,5 @@
 require 'socket'
 require 'json'
-require 'msgpack'
 
 class Client
 
--- visual/visual.rb
+++ visual/visual.rb
@@ -8,13 +8,36 @@ client.connect
 data = client.add_square OpenStruct.new(x: 10, y:10), 10
 body_uuid = data['body_uuid']
 
-puts "data: #{data}"
-puts "body_uuid: #{body_uuid}"
+FPS = 10
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 800
 
-data = client.detail({ body_uuid: body_uuid })
-puts "data: #{data}"
+def to_lt x, y
+  left = (WINDOW_WIDTH/2) + x
+  top  = (WINDOW_HEIGHT/2) + y
+  [left, top]
+end
 
-client.tick
-
-data = client.detail({ body_uuid: body_uuid })
-puts "data: #{data}"
+Shoes.app(width: WINDOW_WIDTH, height: WINDOW_HEIGHT) do
+  stack do
+    animate(FPS) do
+      begin
+        puts "clearing"
+        clear
+        puts "ticking"
+        client.tick
+        puts "getting details"
+        details = client.detail({ body_uuid: body_uuid })
+        puts "details: #{details}"
+        x, y = details['position']['x'], details['position']['y']
+        left, top = to_lt(x, y)
+        width, height = [10, 10]
+        puts "drawing"
+        rect top, left, width, height
+        puts "done drawing"
+      rescue => ex
+        puts "EX: #{ex}"
+      end
+    end
+  end
+end
